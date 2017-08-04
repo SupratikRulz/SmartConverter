b432027b23ea0f805da8b14236ac55b55327ab4e
SupratikRulz <meet.srulz@gmail.com>
2017-08-04 17:03:05 +0530

    Target#2

---
+++ src/digit_text.js
@@ -0,0 +1,87 @@
+var a = ['', 'one ', 'two ', 'three ', 'four ', 'five ', 'six ', 'seven ', 'eight ', 'nine ', 'ten ', 'eleven ', 'twelve ', 'thirteen ', 'fourteen ', 'fifteen ', 'sixteen ', 'seventeen ', 'eighteen ', 'nineteen '];
+var b = ['', '', 'twenty ', 'thirty ', 'forty ', 'fifty ', 'sixty ', 'seventy ', 'eighty ', 'ninety '];
+
+
+var getInWords = function(number) {
+    var word = "";
+    if (number.toString().length > 10) {
+        return "cannot be computed";
+    }
+    if (number % 100 < 21) {
+        word = word + a[number % 100];
+        //  console.log(word);
+    } else if (number % 10 != 0) {
+        word = word + b[Math.floor(number / 10) % 10] + a[number % 10];
+        //console.log("2nd" + word);
+    } else {
+        word = word + b[Math.floor(number / 10) % 10];
+        //console.log("3rd" + word);
+    }
+    /**Upto 99 done */
+    if (Math.floor(number / 100) > 0) {
+        word = a[Math.floor(number / 100) % 10] + "hundred and " + word;
+        //console.log("4th" + word);
+        //console.log(number);
+    }
+
+    number = Math.floor(number / 1000);
+    //console.log("Number after floor " + number);
+    if (number % 100 < 21 && number != 0) {
+        word = a[number % 100] + "thousand " + word;
+        //console.log(word);
+    } else if (number % 10 != 0) {
+        word = b[Math.floor(number / 10) % 10] + a[number % 10] + "thousand " + word;
+        //console.log("2nd" + word);
+    } else if (number != 0) {
+        word = b[Math.floor(number / 10) % 10] + "thousand " + word;
+        //console.log("3rd" + word);
+    }
+    /**Upto 9999 done */
+
+    if (number != 0 && Math.floor(number / 100) > 0) {
+        word = a[Math.floor(number / 100) % 10] + "hundred " + word;
+        //console.log("4th" + word);
+    }
+
+    /**Upto 999,999 done */
+
+    number = Math.floor(number / 1000);
+
+    if (number % 100 < 21 && number != 0) {
+        word = a[number % 100] + "million " + word;
+        //console.log(word);
+    } else if (number % 10 != 0) {
+        word = b[Math.floor(number / 10) % 10] + a[number % 10] + "million " + word;
+        //console.log("2nd" + word);
+    } else if (number != 0) {
+        word = b[Math.floor(number / 10) % 10] + "million " + word;
+        //console.log("3rd" + word);
+    }
+    /**Upto 99,999,999 done */
+
+    if (number != 0 && Math.floor(number / 100) > 0) {
+        word = a[Math.floor(number / 100) % 10] + "hundred " + word;
+        //console.log("4th" + word);
+    }
+    /**Upto 999,999,999 done */
+
+    number = Math.floor(number / 1000);
+
+    if (number % 100 < 21 && number != 0) {
+        word = a[number % 100] + "billion " + word;
+        //console.log(word);
+    } else if (number % 10 != 0) {
+        word = b[Math.floor(number / 10) % 10] + a[number % 10] + "billion " + word;
+        //console.log("2nd" + word);
+    } else if (number != 0) {
+        word = b[Math.floor(number / 10) % 10] + "billion " + word;
+        //console.log("3rd" + word);
+    }
+    /**Upto 99,999,999 done */
+
+    if (number != 0 && Math.floor(number / 100) > 0) {
+        word = a[Math.floor(number / 100) % 10] + "hundred " + word;
+        //console.log("4th" + word);
+    }
+    return word.trim();
+};
--- test/main.test.js
+++ test/main.test.js
@@ -4,9 +4,50 @@ describe("This is getNumber() test", function() {
         getNumber("one");
         expect(window.wordToNumber).toHaveBeenCalled();
     });
+
     it("should return the number in decimal format from the word format", function() {
         expect(getNumber("one hundred and two")).toBe(102);
         expect(getNumber("one thousand fifty")).toBe(1050);
-        //    expect(getNumber("two million and one hundred five thousand two hundred and ninety six")).toBe(2105296);
+        expect(getNumber("two million and one hundred five thousand two hundred and ninety six")).toBe(2105296);
+        expect(getNumber("seven thousand one hundred twenty five")).toBe(7125);
     });
+});
+
+describe("This is wordToNumber() test", function() {
+    it("should change the value of trackerG when word is cointatined in low[]", function() {
+        trackerG = 0;
+        expect(trackerG).toBe(0);
+        wordToNumber("one");
+        expect(trackerG).not.toBe(0);
+    });
+
+    it("should change the value of trackerN when word is cointatined in low[]", function() {
+        trackerN = 0;
+        expect(trackerN).toBe(0);
+        wordToNumber("million");
+        expect(trackerN).not.toBe(0);
+    });
+});
+
+
+describe("This is getInWords() test", function() {
+    it("should give word upto 99", function() {
+        expect(getInWords(9)).toBe("nine");
+        expect(getInWords(10)).toBe("ten");
+        expect(getInWords(19)).toBe("nineteen");
+        expect(getInWords(55)).toBe("fifty five");
+        expect(getInWords(90)).toBe("ninety");
+        expect(getInWords(990)).toBe("nine hundred and ninety");
+        expect(getInWords(999)).toBe("nine hundred and ninety nine");
+        expect(getInWords(9999)).toBe("nine thousand nine hundred and ninety nine");
+        expect(getInWords(99999)).toBe("ninety nine thousand nine hundred and ninety nine");
+        expect(getInWords(999999)).toBe("nine hundred ninety nine thousand nine hundred and ninety nine");
+        expect(getInWords(9999999)).toBe("nine million nine hundred ninety nine thousand nine hundred and ninety nine");
+        expect(getInWords(99999999)).toBe("ninety nine million nine hundred ninety nine thousand nine hundred and ninety nine");
+        expect(getInWords(999999999)).toBe("nine hundred ninety nine million nine hundred ninety nine thousand nine hundred and ninety nine");
+        expect(getInWords(9999999999)).toBe("nine billion nine hundred ninety nine million nine hundred ninety nine thousand nine hundred and ninety nine");
+        expect(getInWords(99999999999)).toBe("cannot be computed");
+        expect(getInWords(7125)).toBe("seven thousand one hundred and twenty five");
+    });
+
 });
